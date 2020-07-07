3286628538005724e95b510f6da167c8536c20d3
Kevin Park <kevin.park07@yahoo.com>
2020-07-07 17:28:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 def loop_message_five_times (array)
   puts array[0]
-  puts array[1]
-  puts array[2]
-  puts array[3]
-  puts array[4]
+  puts array[0]
+  puts array[0]
+  puts array[0]
+  puts array[0]
 end
